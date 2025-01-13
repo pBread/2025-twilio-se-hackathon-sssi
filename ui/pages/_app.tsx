78517bcef9baf4c104c177e06913113c1f1b54fa
pBread <4473570+pBread@users.noreply.github.com>
2025-01-13 11:25:33 -0700

    added redux boiler

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,12 +1,22 @@
 import { Helmet } from "@/components/Helmet";
+import { type AppStore, makeStore } from "@/state/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRef } from "react";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const storeRef = useRef<AppStore | null>(null);
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
   return (
     <>
       <Helmet />
-      <Main Component={Component} {...pageProps} />
+      <Provider store={storeRef.current}>
+        <Main Component={Component} {...pageProps} />
+      </Provider>
     </>
   );
 }
