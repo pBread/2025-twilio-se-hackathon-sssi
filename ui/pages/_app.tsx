53916307d3df48402cf62a9370a3b62eaac38d2c
pBread <4473570+pBread@users.noreply.github.com>
2025-01-13 11:13:25 -0700

    ui – deleted boilerplate

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,6 +1,20 @@
+import { Helmet } from "@/components/Helmet";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Helmet />
+      <Main Component={Component} {...pageProps} />
+    </>
+  );
+}
+
+function Main({ Component, pageProps }: AppProps) {
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
 }
