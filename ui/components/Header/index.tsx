d9eb71a0c34531e26f96069d1f3cf0e384ee0312
pBread <4473570+pBread@users.noreply.github.com>
2025-01-13 18:21:33 -0700

    working through style

--- ui/components/Header/index.tsx
+++ ui/components/Header/index.tsx
@@ -1,18 +1,24 @@
 import { useSyncSlice } from "@/state/sync";
-import { Text } from "@mantine/core";
+import { Loader } from "@mantine/core";
 
 export function Header() {
-  const syncSlice = useSyncSlice();
-
   return (
     <header>
-      <div className="header-section"></div>
-
       <img className="header-logo" alt="logo" src={"/logo.png"} />
 
+      <div className="header-section"></div>
+
       <div className="header-section">
-        <Text size="sm">Sync: {syncSlice.connectionState}</Text>
+        <Connection />
       </div>
     </header>
   );
+}
+
+function Connection() {
+  const syncSlice = useSyncSlice();
+
+  if (syncSlice.connectionState === "connected") return;
+
+  return <Loader size="sm" />;
 }
