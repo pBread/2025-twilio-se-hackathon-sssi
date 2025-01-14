fdb2f390653c240eaee57c0a3b67f83a65c64272
pBread <4473570+pBread@users.noreply.github.com>
2025-01-14 13:20:05 -0700

    ported mock-database

---
+++ server/services/database-service.ts
@@ -0,0 +1,317 @@
+import twilio from "twilio";
+import type { SyncMapContext } from "twilio/lib/rest/sync/v1/service/syncMap";
+import type {
+  EventRecord,
+  OrderRecord,
+  UserRecord,
+} from "../../shared/entities";
+import {
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  TWILIO_SYNC_SVC_SID,
+} from "../env";
+import log from "../logger";
+import { makeId } from "../util";
+
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+export class DatabaseService {
+  constructor() {
+    this.eventSvc = new MockEntityService<EventRecord, AddEvent>(
+      "events",
+      (param) => ({ id: makeId("ev"), ...param })
+    );
+
+    this.orderSvc = new MockEntityService<OrderRecord, AddOrder>(
+      "orders",
+      (param) => ({ id: makeId("or"), ...param })
+    );
+
+    this.userSvc = new MockEntityService<UserRecord, AddUser>(
+      "users",
+      (param) => ({
+        id: makeId("us"),
+        membership: "none",
+        paymentMethods: [],
+        ...param,
+      })
+    );
+  }
+
+  init = async () => {
+    console.log("initializing database services");
+    await Promise.all([
+      this.eventSvc.init(),
+      this.orderSvc.init(),
+      this.userSvc.init(),
+    ]);
+
+    return this;
+  };
+
+  /****************************************************
+   Event Methods
+  ****************************************************/
+  eventSvc: EntityService<EventRecord, AddEvent>;
+  events = {
+    add: async (doc: AddEvent) => {
+      const result = await this.eventSvc.add(doc);
+      return result;
+    },
+    getById: async (id: string) => {
+      const result = await this.eventSvc.getById(id);
+      return result;
+    },
+    list: async () => {
+      const result = await this.eventSvc.list();
+      return result;
+    },
+    remove: async (id: string) => {
+      const result = await this.eventSvc.remove(id);
+      return result;
+    },
+    set: async (id: string, doc: EventRecord) => {
+      const result = await this.eventSvc.set(id, doc);
+      return result;
+    },
+    setIn: async (id: string, update: Partial<EventRecord>) => {
+      const result = await this.eventSvc.setIn(id, update);
+      return result;
+    },
+  };
+
+  /****************************************************
+   Order Methods
+  ****************************************************/
+  orderSvc: EntityService<OrderRecord, AddOrder>;
+  orders = {
+    add: async (doc: AddOrder) => {
+      log.warn(
+        `db.orders`,
+        `order.add was invoked but database is not connected`,
+        doc
+      );
+      return doc;
+    },
+
+    getById: async (id: string) => {
+      const result = await this.orderSvc.getById(id);
+      return result;
+    },
+    list: async () => {
+      const result = await this.orderSvc.list();
+      return result;
+    },
+    remove: async (id: string) => {
+      const result = await this.orderSvc.remove(id);
+      return result;
+    },
+    set: async (id: string, doc: OrderRecord) => {
+      const result = await this.orderSvc.set(id, doc);
+      return result;
+    },
+
+    setIn: async (id: string, update: Partial<OrderRecord>) => {
+      const result = await this.orderSvc.setIn(id, update);
+      return result;
+    },
+  };
+
+  /****************************************************
+   User Methods
+  ****************************************************/
+  userSvc: EntityService<UserRecord, AddUser>;
+  users = {
+    add: async (doc: AddUser) => {
+      const result = await this.userSvc.add(doc);
+      return result;
+    },
+
+    getById: async (id: string) => {
+      const result = await this.userSvc.getById(id);
+      return result;
+    },
+    list: async () => {
+      const result = await this.userSvc.list();
+      return result;
+    },
+    remove: async (id: string) => {
+      const result = await this.userSvc.remove(id);
+      return result;
+    },
+    set: async (id: string, doc: UserRecord) => {
+      const result = await this.userSvc.set(id, doc);
+      return result;
+    },
+
+    setIn: async (id: string, update: Partial<UserRecord>) => {
+      const result = await this.userSvc.setIn(id, update);
+      return result;
+    },
+
+    getByChannel: async (phoneOrEmail: string) => {
+      const users = await this.userSvc.list();
+
+      return users.find(
+        (user) =>
+          user.mobilePhone?.includes(phoneOrEmail) ||
+          user.email?.includes(phoneOrEmail)
+      );
+    },
+  };
+}
+
+type AddEvent = Omit<EventRecord, "id"> & { id?: string };
+type AddOrder = Omit<OrderRecord, "id"> & { id?: string };
+type AddUser = Partial<UserRecord> & { firstName: string; lastName: string };
+
+/****************************************************
+ Mock Entity Service
+****************************************************/
+interface EntityService<T extends { id: string }, ADD extends Partial<T>> {
+  add: (partial: ADD) => Promise<T>;
+  getById: (id: string) => Promise<T | undefined>;
+  init: () => Promise<void>;
+  list: () => Promise<T[]>;
+  remove: (id: string) => Promise<boolean>;
+  set: (id: string, doc: T) => Promise<T>;
+  setIn: (id: string, update: Partial<T>) => Promise<T>;
+}
+
+class MockEntityService<T extends { id: string }, ADD extends Partial<T>>
+  implements EntityService<T, ADD>
+{
+  docs: T[];
+  constructor(public name: string, private creator: (param: ADD) => T) {
+    this.docs = [];
+  }
+
+  init = async () => {
+    const mockDatabase = await import("../../shared/mock-database").then(
+      (it) => it.mockDatabase
+    );
+    // @ts-ignore
+    this.docs = mockDatabase[name] as T[];
+    log.warn(
+      `mock.${this.name}`,
+      `initializing mock ${this.name} service. insert operations will not function.`
+    );
+  };
+
+  add = async (partial: ADD) => {
+    const doc = this.creator(partial);
+    log.warn(
+      `mock.${this.name}`,
+      `attempted to add ${this.name}, but this entity is using the mockdatabase`,
+      doc
+    );
+
+    return doc;
+  };
+
+  getById = async (id: string) => this.docs.find((doc) => doc.id === id);
+
+  list = async () => [...this.docs];
+  remove = async (id: string) => {
+    log.warn(
+      `mock.${this.name}`,
+      `attempted to remove ${this.name} with id ${id}, but this entity is using the mockdatabase`
+    );
+
+    return true;
+  };
+  set = async (id: string, doc: T) => {
+    log.warn(
+      `mock.${this.name}`,
+      `attempted to set ${this.name}, but this entity is using the mockdatabase`,
+      id,
+      doc
+    );
+
+    return doc;
+  };
+
+  setIn = async (id: string, update: Partial<T>) => {
+    const cur = await this.getById(id);
+    if (!cur) {
+      const msg = `attempted to update ${this.name} but could not find id ${id}`;
+      log.error(`mock.${this.name}`, msg);
+      throw Error(msg);
+    }
+
+    const next = { ...cur, ...update };
+
+    log.warn(
+      `mock.${this.name}`,
+      `attempted to setIn ${this.name}, but this entity is using the mockdatabase`,
+
+      next
+    );
+
+    return next;
+  };
+}
+
+/****************************************************
+ Sync Service Entity
+****************************************************/
+class SyncEntityService<T extends { id: string }, P extends Partial<T>>
+  implements EntityService<T, P>
+{
+  map: SyncMapContext;
+
+  constructor(public name: string, private creator: (param: P) => T) {
+    this.map = client.sync.v1.services(TWILIO_SYNC_SVC_SID).syncMaps(this.name);
+  }
+
+  init = async () => {
+    try {
+      // create map if it doesn't exist
+      const map = await client.sync.v1
+        .services(TWILIO_SYNC_SVC_SID)
+        .syncMaps.create({ uniqueName: this.name });
+
+      if (map) console.log(`created sync map ${this.name}`);
+      else throw Error(`Error creating Sync Map for ${this.name}`);
+    } catch (error) {
+      if ((error as { code?: number })?.code === 54301) return; // ignore sync map already exists
+
+      log.error(`sync.${this.name}`, "Error creating Sync Map", error);
+    }
+  };
+
+  add = async (partial: P) => {
+    const data = this.creator(partial);
+    const result = await this.map.syncMapItems.create({ key: data.id, data });
+    return data as T;
+  };
+
+  getById = async (id: string) => {
+    const result = await this.map.syncMapItems(id).fetch();
+    return result.data as T;
+  };
+
+  list = async () => {
+    const docs = await this.map.syncMapItems.list();
+    return docs.map((doc) => doc.data) as T[];
+  };
+
+  remove = async (id: string) => {
+    const result = await this.map.syncMapItems(id).remove();
+    return result;
+  };
+
+  set = async (id: string, doc: T) => {
+    await this.map.syncMapItems(id).update({ data: doc });
+    const data = await this.getById(id);
+    return data as T;
+  };
+
+  setIn = async (id: string, update: Partial<T>) => {
+    const cur = await this.getById(id);
+    const next = { ...cur, ...update };
+    await this.map.syncMapItems(id).update({ data: next });
+
+    return next as T;
+  };
+}
