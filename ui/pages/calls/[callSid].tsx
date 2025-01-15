90f5e442b6d147779254a3950fd307c3f3c55ace
pBread <4473570+pBread@users.noreply.github.com>
2025-01-14 19:39:43 -0700

    added table

--- ui/pages/calls/[callSid].tsx
+++ ui/pages/calls/[callSid].tsx
@@ -1,14 +1,12 @@
 import { selectCallById } from "@/state/calls";
 import { useAppSelector } from "@/state/hooks";
 import { getCallLogs } from "@/state/logs";
-import { getCallMessages } from "@/state/messages";
-import { JsonInput, Paper, Title } from "@mantine/core";
+import { getCallMessages, getMessageById } from "@/state/messages";
+import { JsonInput, Paper, Table, Title } from "@mantine/core";
+import { BotMessage, HumanMessage } from "@shared/entities";
 import { useRouter } from "next/router";
 
 export default function LiveCall() {
-  const router = useRouter();
-  const callSid = router.query.callSid as string;
-
   return (
     <div style={{ display: "flex", gap: "8px" }}>
       <div style={{ flex: 2 }}>
@@ -26,6 +24,8 @@ function Conscious() {
     <Paper>
       <Title order={3}>Conscious Bot</Title>
 
+      <Title order={4}>Turns</Title>
+      <MessageTable />
       <MessagesList />
       <LogsList />
     </Paper>
@@ -36,8 +36,67 @@ function MessageTable() {
   const router = useRouter();
   const callSid = router.query.callSid as string;
 
-  const msgs = useAppSelector;
+  const msgs = useAppSelector((state) => getCallMessages(state, callSid));
+
+  return (
+    <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Td>Role</Table.Td>
+          <Table.Td>Type</Table.Td>
+          <Table.Td>Content</Table.Td>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>
+        {msgs.map((msg) => (
+          <Table.Tr key={`di8-${msg.id}`}>
+            {msg.role === "bot" && <BotRow msgId={msg.id} />}
+            {msg.role === "human" && <HumanRow msgId={msg.id} />}
+          </Table.Tr>
+        ))}
+      </Table.Tbody>
+    </Table>
+  );
 }
+
+function BotRow({ msgId }: { msgId: string }) {
+  const msg = useAppSelector((state) =>
+    getMessageById(state, msgId)
+  ) as BotMessage;
+
+  let content = "";
+
+  if (msg.type === "tool") {
+    const tool = msg.tool_calls[0];
+    content = `${tool.function.name}(${JSON.stringify(
+      tool.function.arguments
+    )})`.replaceAll("\\", "");
+  } else content = msg.content;
+
+  return (
+    <>
+      <Table.Th> {msg.role}</Table.Th>
+      <Table.Th> {msg.type}</Table.Th>
+      <Table.Th> {content}</Table.Th>
+    </>
+  );
+}
+
+function HumanRow({ msgId }: { msgId: string }) {
+  const msg = useAppSelector((state) =>
+    getMessageById(state, msgId)
+  ) as HumanMessage;
+
+  return (
+    <>
+      <Table.Th> {msg.role}</Table.Th>
+      <Table.Th> {msg.type}</Table.Th>
+      <Table.Th> {msg.content}</Table.Th>
+    </>
+  );
+}
+
+function SystemRow({ msgId }: { msgId: string }) {}
 
 function MessagesList() {
   const router = useRouter();
